e73d06002ea98782c2990fa4f6360c7542da3556
MatheusAfons <107150975+MatheusAfons@users.noreply.github.com>
2023-08-17 18:49:15 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,138 +1,137 @@
 export default [
-    {
-        question: "Qual é a linguagem de marcação usada para estruturar o conteúdo de uma página da web?",
-        answers: [
-        { option: "CSS", correct: false },
-        { option: "HTML", correct: true },
-        { option: "JavaScript", correct: false },
-        { option: "XML", correct: false },
-        ],
-    },
-    {
-        question: "Qual operador em JavaScript verifica a igualdade de valor e tipo de dado?",
-        answers: [
-        { option: "==", correct: false },
-        { option: "===", correct: true },
-        { option: "=", correct: false },
-        { option: "!=", correct: false },
-        ],
-    },
-    {
-        question: "Qual estrutura de controle permite executar um bloco de código várias vezes com base em uma condição?",
-        answers: [
-        { option: "if", correct: false },
-        { option: "for", correct: true },
-        { option: "switch", correct: false },
-        { option: "while", correct: false },
-        ],
-    },
-    {
-        question: "O que é um objeto em programação orientada a objetos?",
-        answers: [
-        { option: "Um valor primitivo", correct: false },
-        { option: "Um loop", correct: false },
-        { option: "Uma estrutura de dados", correct: false },
-        { option: "Uma instância de uma classe", correct: true },
-        ],
-    },
-    {
-        question: "Qual operador lógico é usado para verificar se duas ou mais condições são verdadeiras ao mesmo tempo?",
-        answers: [
-        { option: "||", correct: false },
-        { option: "&&", correct: true },
-        { option: "!", correct: false },
-        { option: ">", correct: false },
-        ],
-    },
-    {
-        question: "Qual termo é usado para armazenar e recuperar dados em um sistema de gerenciamento de banco de dados SQL?",
-        answers: [
-        { option: "Script", correct: false },
-        { option: "Arquivo", correct: false },
-        { option: "Registro", correct: false },
-        { option: "Consulta", correct: true },
-        ],
-    },
-    {
-        question: "Qual declaração de variável em JavaScript não pode ser reatribuída após a inicialização?",
-        answers: [
-        { option: "var", correct: false },
-        { option: "const", correct: true },
-        { option: "let", correct: false },
-        { option: "def", correct: false },
-        ],
-    },
-    {
-        question: "Qual componente de software hospeda e serve páginas da web para os navegadores dos usuários?",
-        answers: [
-        { option: "Navegador", correct: false },
-        { option: "Servidor web", correct: true },
-        { option: "Protocolo HTTP", correct: false },
-        { option: "Sistema operacional", correct: false },
-        ],
-    },
-    {
-        question: "Qual paradigma de programação organiza o código em objetos com propriedades e métodos?",
-        answers: [
-        { option: "Programação imperativa", correct: false },
-        { option: "Programação funcional", correct: false },
-        { option: "Programação orientada a objetos", correct: true },
-        { option: "Programação procedural", correct: false },
-        ],
-    },
-    {
-        question: "Qual sistema é usado para controlar as alterações feitas em um código-fonte ao longo do tempo?",
-        answers: [
-        { option: "Versionamento de código", correct: true },
-        { option: "Controle de acesso", correct: false },
-        { option: "Rastreamento de problemas", correct: false },
-        { option: "Automação de teste", correct: false },
-        ],
-    },
-    {
-        question: "Qual é a linguagem de programação mais utilizada para desenvolvimento web?",
-        answers: [
-        { option: "Python", correct: false },
-        { option: "JavaScript", correct: true },
-        { option: "Java", correct: false },
-        { option: "C++", correct: false },
-        ],
-    },
-    {
-        question: "O que significa a sigla HTML?",
-        answers: [
-        { option: "HyperText Markup Language", correct: true },
-        { option: "High-Level Text Mode Language", correct: false },
-        { option: "HyperLink and Text Markup Language", correct: false },
-        { option: "Home Tool Markup Language", correct: false },
-        ],
-    },
-    {
-        question: "Qual é o método em JavaScript usado para adicionar um elemento no final de um array?",
-        answers: [
-        { option: "push()", correct: true },
-        { option: "pop()", correct: false },
-        { option: "splice()", correct: false },
-        { option: "shift()", correct: false },
-        ],
-    },
-    {
-        question: "O que CSS significa em programação web?",
-        answers: [
-        { option: "Computer Style System", correct: false },
-        { option: "Cascading Style Sheets", correct: true },
-        { option: "Coding and Styling System", correct: false },
-        { option: "Creative Styling Solutions", correct: false },
-        ],
-    },
-    {
-        question: "Qual é a linguagem de programação usada para criar páginas web interativas?",
-        answers: [
-        { option: "Python", correct: false },
-        { option: "Java", correct: false },
-        { option: "JavaScript", correct: true },
-        { option: "C#", correct: false },
-        ],
-    },
-    
-    ];
+  {
+    question: "Qual é a linguagem de marcação usada para estruturar o conteúdo de uma página da web?",
+    answers: [
+      { option: "A) CSS", correct: false },
+      { option: "B) HTML", correct: true },
+      { option: "C) JavaScript", correct: false },
+      { option: "D) XML", correct: false },
+    ],
+  },
+  {
+    question: "Qual operador em JavaScript verifica a igualdade de valor e tipo de dado?",
+    answers: [
+      { option: "A) ==", correct: false },
+      { option: "B) ===", correct: true },
+      { option: "C) =", correct: false },
+      { option: "D) !=", correct: false },
+    ],
+  },
+  {
+    question: "Qual estrutura de controle permite executar um bloco de código várias vezes com base em uma condição?",
+    answers: [
+      { option: "A) if", correct: false },
+      { option: "B) for", correct: true },
+      { option: "C) switch", correct: false },
+      { option: "D) while", correct: false },
+    ],
+  },
+  {
+    question: "O que é um objeto em programação orientada a objetos?",
+    answers: [
+      { option: "A) Um valor primitivo", correct: false },
+      { option: "B) Um loop", correct: false },
+      { option: "C) Uma estrutura de dados", correct: false },
+      { option: "D) Uma instância de uma classe", correct: true },
+    ],
+  },
+  {
+    question: "Qual operador lógico é usado para verificar se duas ou mais condições são verdadeiras ao mesmo tempo?",
+    answers: [
+      { option: "A) ||", correct: false },
+      { option: "B) &&", correct: true },
+      { option: "C) !", correct: false },
+      { option: "D) >", correct: false },
+    ],
+  },
+  {
+    question: "Qual termo é usado para armazenar e recuperar dados em um sistema de gerenciamento de banco de dados SQL?",
+    answers: [
+      { option: "A) Script", correct: false },
+      { option: "B) Arquivo", correct: false },
+      { option: "C) Registro", correct: false },
+      { option: "D) Consulta", correct: true },
+    ],
+  },
+  {
+    question: "Qual declaração de variável em JavaScript não pode ser reatribuída após a inicialização?",
+    answers: [
+      { option: "A) var", correct: false },
+      { option: "B) const", correct: true },
+      { option: "C) let", correct: false },
+      { option: "D) def", correct: false },
+    ],
+  },
+  {
+    question: "Qual componente de software hospeda e serve páginas da web para os navegadores dos usuários?",
+    answers: [
+      { option: "A) Navegador", correct: false },
+      { option: "B) Servidor web", correct: true },
+      { option: "C) Protocolo HTTP", correct: false },
+      { option: "D) Sistema operacional", correct: false },
+    ],
+  },
+  {
+    question: "Qual paradigma de programação organiza o código em objetos com propriedades e métodos?",
+    answers: [
+      { option: "A) Programação imperativa", correct: false },
+      { option: "B) Programação funcional", correct: false },
+      { option: "C) Programação orientada a objetos", correct: true },
+      { option: "D) Programação procedural", correct: false },
+    ],
+  },
+  {
+    question: "Qual sistema é usado para controlar as alterações feitas em um código-fonte ao longo do tempo?",
+    answers: [
+      { option: "A) Versionamento de código", correct: true },
+      { option: "B) Controle de acesso", correct: false },
+      { option: "C) Rastreamento de problemas", correct: false },
+      { option: "D) Automação de teste", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a linguagem de programação mais utilizada para desenvolvimento web?",
+    answers: [
+      { option: "A) Python", correct: false },
+      { option: "B) JavaScript", correct: true },
+      { option: "C) Java", correct: false },
+      { option: "D) C++", correct: false },
+    ],
+  },
+  {
+    question: "O que significa a sigla HTML?",
+    answers: [
+      { option: "A) HyperText Markup Language", correct: true },
+      { option: "B) High-Level Text Mode Language", correct: false },
+      { option: "C) HyperLink and Text Markup Language", correct: false },
+      { option: "D) Home Tool Markup Language", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o método em JavaScript usado para adicionar um elemento no final de um array?",
+    answers: [
+      { option: "A) push()", correct: true },
+      { option: "B) pop()", correct: false },
+      { option: "C) splice()", correct: false },
+      { option: "D) shift()", correct: false },
+    ],
+  },
+  {
+    question: "O que CSS significa em programação web?",
+    answers: [
+      { option: "A) Computer Style System", correct: false },
+      { option: "B) Cascading Style Sheets", correct: true },
+      { option: "C) Coding and Styling System", correct: false },
+      { option: "D) Creative Styling Solutions", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a linguagem de programação usada para criar páginas web interativas?",
+    answers: [
+      { option: "A) Python", correct: false },
+      { option: "B) Java", correct: false },
+      { option: "C) JavaScript", correct: true },
+      { option: "D) C#", correct: false },
+    ],
+  },
+];
